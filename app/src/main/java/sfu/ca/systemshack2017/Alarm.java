90c825674c8c1c46eab9f8c8b7e84e6639028af3
Tim Robertson <timr@sfu.ca>
2017-01-21 10:38:03 -0800

    Added CreateAlarm activity

---
+++ app/src/main/java/sfu/ca/systemshack2017/Alarm.java
@@ -0,0 +1,13 @@
+package sfu.ca.systemshack2017;
+
+/**
+ * Created by timr on 2017-01-21.
+ */
+
+public class Alarm {
+
+    public Alarm() {
+
+    }
+
+}
---
+++ app/src/main/java/sfu/ca/systemshack2017/CreateAlarm.java
@@ -0,0 +1,52 @@
+package sfu.ca.systemshack2017;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class CreateAlarm extends AppCompatActivity {
+
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_alarm);
+
+        startAlertAtParticularTime();
+
+
+    }
+
+
+
+    public void startAlertAtParticularTime() {
+
+        // alarm first vibrate at 14 hrs and 40 min and repeat itself at ONE_HOUR interval
+
+        intent = new Intent(this, AlarmBroadcastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 280192, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 14);
+        calendar.set(Calendar.MINUTE, 40);
+
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_HOUR, pendingIntent);
+
+        Toast.makeText(this, "Alarm will vibrate at time specified",
+                Toast.LENGTH_SHORT).show();
+
+    }
+}
