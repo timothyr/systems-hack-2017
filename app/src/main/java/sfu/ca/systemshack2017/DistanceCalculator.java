30fba1d0e52fc938a027bdc2d5943806bd5ed7c5
Joe <hei_hei_pig@hotmail.com>
2017-01-21 13:30:52 -0800

    test

---
+++ app/src/main/java/sfu/ca/systemshack2017/DistanceCalculator.java
@@ -0,0 +1,9 @@
+package sfu.ca.systemshack2017;
+
+/**
+ * Created by Joe on 1/21/2017.
+ */
+
+public class DistanceCalculator {
+
+}
--- app/src/main/java/sfu/ca/systemshack2017/MainActivity.java
+++ app/src/main/java/sfu/ca/systemshack2017/MainActivity.java
@@ -1,15 +1,30 @@
 package sfu.ca.systemshack2017;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+
+import junit.framework.Assert;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setupServices();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,9 +42,52 @@ public class MainActivity extends AppCompatActivity {
                 //TODO alarm activity
                 Snackbar.make(view, "TODO add alarm", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
+
+    public void setupServices() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("Connection", "Connection Ready");
+
+        Assert.assertTrue(mGoogleApiClient.isConnected());
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location != null) {
+            Toast.makeText(this, location.toString(), Toast.LENGTH_LONG);
+        } else {
+            Log.d("NullLocation", "Location is null");
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle
+        // the failure silently
+        Log.d("Connection failed", "" + result.getErrorCode());
+        Toast.makeText(this, "Google Play Service connection failed. " + result.getErrorMessage(), Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
