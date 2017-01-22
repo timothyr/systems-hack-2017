6c79327ced6eac5023d1eb942174e39f5a197d0b
Tim Robertson <timr@sfu.ca>
2017-01-21 18:34:55 -0800

    Add EventScheduler

--- app/src/main/java/sfu/ca/systemshack2017/alarm/AlarmService.java
+++ app/src/main/java/sfu/ca/systemshack2017/alarm/AlarmService.java
@@ -1,8 +1,11 @@
 package sfu.ca.systemshack2017.alarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
@@ -21,6 +24,36 @@ public class AlarmService extends Service {
     public void onCreate() {
         Log.d(this.getClass().getSimpleName(),"onCreate()");
         super.onCreate();
+
+        updateEventScheduler();
+    }
+
+    public void updateEventScheduler() {
+        int updateFrequencyInMillis = 30 * 1000;
+
+        // the best number
+        long id = -999;
+
+        // Package intent
+        Intent alarmIntent = new Intent(getApplicationContext(), AlarmBroadcastReceiver.class);
+        alarmIntent.putExtra("updateevents", id);
+
+        //TO:DO different requestcode for each alarm
+        int requestCode = -999;
+
+        // Create new alarm intent
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                getApplicationContext(),
+                requestCode,
+                alarmIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        //PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getApplicationContext().getSystemService(ALARM_SERVICE);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), updateFrequencyInMillis, pendingIntent);
+
+        Log.d("Alarm Event Scheduler", "Set for every " + updateFrequencyInMillis/1000 + " seconds");
     }
 
     @Override
